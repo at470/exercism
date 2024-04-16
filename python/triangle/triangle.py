eb2018f059dd558c0ae5ea6e8a1540f23a526983
Akiko <kaytodayo@gmail.com>
2024-04-16 15:27:57 +0100

    prep armstrong numbers

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,29 @@
 def equilateral(sides):
-    pass
-
-
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+    is_equilateral = False
+    if a == b and b == c and a != 0:
+        is_equilateral = True
+    return is_equilateral
 def isosceles(sides):
-    pass
-
-
-def scalene(sides):
-    pass
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+    is_isosceles = False
+    if equilateral(sides) == True:
+        is_isosceles = True
+    if (a != b and b == c) or (a == b and a != c) or (a == c and a != b):
+         if a + b >= c and b + c >= a and a + c >= b:
+            is_isosceles = True
+    return is_isosceles
+def scalene(sides):  
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+    is_scalene = False
+    if equilateral(sides) == False:
+        if isosceles(sides) == False:
+            if a + b >= c and b + c >= a and a + c >= b:
+                is_scalene = True
+    return is_scalene
