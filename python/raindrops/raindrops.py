b84dc264d5b7f147b60b0739fe481773fd3dbdfe
Akiko <kaytodayo@gmail.com>
2024-04-17 16:41:10 +0100

    raindrops done

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,19 @@
 def convert(number):
-    pass
+    """
+    If a given number:
+        is divisible by 3, add "Pling" to the result.
+        is divisible by 5, add "Plang" to the result.
+        is divisible by 7, add "Plong" to the result.
+        is not divisible by 3, 5, or 7, the result 
+        should be the number as a string.
+    """
+    string = ''
+    if number % 3 != 0 and number % 5 != 0 and number % 7 != 0:
+        string = string + str(number)
+    if number % 3 is 0:
+        string = string + 'Pling'
+    if number % 5 is 0:
+        string = string + 'Plang'
+    if number % 7 is 0:
+        string = string + 'Plong'
+    return string
