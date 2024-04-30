2b682c825312451ba2b52b71342b6d7a2c529de1
Akiko <kaytodayo@gmail.com>
2024-04-30 14:29:25 +0100

    resistor exercises

---
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,17 @@
+def value(colors):
+    colour_number_lookup = {
+        'black' : '0',
+        'brown' : '1',
+        'red' : '2',
+        'orange' : '3',
+        'yellow' : '4',
+        'green' : '5',
+        'blue' : '6',
+        'violet' : '7',
+        'grey' : '8',
+        'white' : '9'
+        }
+    
+    value = int(colour_number_lookup[colors[0]] + colour_number_lookup[colors[1]])
+    
+    return value
