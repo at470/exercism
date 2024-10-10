3ea08414204817a7034b604dc889facf76e4ea6a
Akiko <kaytodayo@gmail.com>
2024-10-10 19:09:02 +0100

    untidy twelve days

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -1,3 +1,45 @@
-def convert(input_grid):
-    pass
+def check_input(input_grid):
+    if len(input_grid)%4 == 0:
+        for row in input_grid:
+            if len(row)%3 == 0:
+                #checks have passed
+                return True
+            else:
+                raise ValueError("Number of input columns is not a multiple of three")
+    raise ValueError("Number of input lines is not a multiple of four")
 
+def check_one_digit(input_grid):
+    result = '?'
+    # check for 3 x 4
+    if check_input(input_grid):
+        if input_grid == [" _ ", "| |", "|_|", "   "]:
+            result = '0'
+        elif input_grid == ["   ", "  |", "  |", "   "]:
+            result = '1'
+        elif input_grid == [" _ ", " _|", "|_ ", "   "]:
+            result = '2'
+        elif input_grid == [" _ ", " _|", " _|", "   "]:
+            result = '3'
+        elif input_grid == ["   ", "|_|", "  |", "   "]:
+            result = '4'
+        elif input_grid == [" _ ", "|_ ", " _|", "   "]:
+            result = '5'
+        elif input_grid == [" _ ", "|_ ", "|_|", "   "]:
+            result = '6'
+        elif input_grid == [" _ ", "  |", "  |", "   "]:
+            result = '7'
+        elif input_grid == [" _ ", "|_|", "|_|", "   "]:
+            result = '8'
+        elif input_grid == [" _ ", "|_|", " _|", "   "]:
+            result = '9'
+    return result
+
+def split_string(s, chunk_size):
+    return [s[i:i+chunk_size] for i in range(0, len(s), chunk_size)]
+    
+def convert(input_grid):
+    split_row = []
+    for row in input_grid:
+        split_row.append(split_string(row,3))
+    print(split_row)
+    return check_one_digit(input_grid)
---
+++ python/twelve-days/twelve_days.py
@@ -0,0 +1,94 @@
+def initialise_gift_variable(start_verse):
+    gift_dict = {1 : 'and a Partridge in a Pear Tree',
+                2 : 'two Turtle Doves, ',
+                3 : 'three French Hens, ',
+                4 : 'four Calling Birds, ',
+                5 : 'five Gold Rings, ',
+                6 : 'six Geese-a-Laying, ',
+                7 : 'seven Swans-a-Swimming, ', 
+                8 : 'eight Maids-a-Milking, ',
+                9 : 'nine Ladies Dancing, ',
+                10 : 'ten Lords-a-Leaping, ',
+                11 : 'eleven Pipers Piping, ',
+                12 : 'twelve Drummers Drumming, '}
+    # construct initial list of gifts
+    gifts_list_string = ''
+    loop_counter = 1
+    while loop_counter <= start_verse:
+        gifts_list_string = gift_dict[loop_counter] + gifts_list_string
+        loop_counter += 1
+    return gifts_list_string
+
+def create_verse():
+    
+def recite(start_verse, end_verse):
+    returned_lyrics = []
+    verse_dict = {1 : 'first',
+                 2 : 'second',
+                 3 : 'third',
+                 4 : 'fourth',
+                 5 : 'fifth',
+                 6 : 'sixth',
+                 7 : 'seventh',
+                 8 : 'eighth',
+                 9 : 'ninth',
+                  10 : 'tenth',
+                  11 : 'eleventh',
+                  12 : 'twelfth'}
+    
+    gift_dict = {1 : 'and a Partridge in a Pear Tree',
+                2 : 'two Turtle Doves, ',
+                3 : 'three French Hens, ',
+                4 : 'four Calling Birds, ',
+                5 : 'five Gold Rings, ',
+                6 : 'six Geese-a-Laying, ',
+                7 : 'seven Swans-a-Swimming, ', 
+                8 : 'eight Maids-a-Milking, ',
+                9 : 'nine Ladies Dancing, ',
+                10 : 'ten Lords-a-Leaping, ',
+                11 : 'eleven Pipers Piping, ',
+                12 : 'twelve Drummers Drumming, '}
+    
+    first_verse_only = f'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.'
+
+    # initialise list of gifts
+    list_of_gifts = initialise_gift_variable(start_verse)
+    
+    # first verse only
+    if start_verse == 1 and end_verse == 1:
+        verse = first_verse_only
+        returned_lyrics.append(verse)
+    # when returning one verse only, not first verse
+    elif start_verse - end_verse == 0:
+    # construct initial value of gifts
+        verse = f'On the {verse_dict[start_verse]} day of Christmas my true love gave to me: {list_of_gifts}.'
+        returned_lyrics.append(verse)
+    # multiple verses, *not* starting first verse
+    elif start_verse > 1:
+        counter = start_verse
+        while counter <= end_verse:
+            verse = f'On the {verse_dict[counter]} day of Christmas my true love gave to me: {list_of_gifts}.'
+            returned_lyrics.append(verse)
+            counter += 1
+            # increment list of gifts for next time
+            list_of_gifts = gift_dict[counter] + list_of_gifts
+    
+    else:
+        if start_verse == 1:
+            verse = first_verse_only
+            returned_lyrics.append(verse)
+
+        loop_count = 2
+        list_of_gifts_from_second_verse = initialise_gift_variable(2)
+
+        while loop_count <= end_verse:
+            verse = f'On the {verse_dict[loop_count]} day of Christmas my true love gave to me: {list_of_gifts_from_second_verse}.'
+            returned_lyrics.append(verse)
+            loop_count += 1
+            # increment list of gifts for next time
+            if loop_count > end_verse:
+                pass
+            else:
+                list_of_gifts_from_second_verse = gift_dict[loop_count] + list_of_gifts_from_second_verse
+    return returned_lyrics
+
